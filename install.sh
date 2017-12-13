732b3e3969fc39f37fd0b1293997f419c6392314
Vladislav Nikonov <pacmancoder@gmail.com>
2017-12-13 23:12:44 +0200

    Added zshrc, modified install script

--- install.sh
+++ install.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 
+# check root access
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+HOME=$(eval echo "~$SUDO_USER")
+
+USE_SYMLINK=true
+CONFIG_STORAGE="$HOME/core/configs"
+
+function link_config {
+    [ -e $2 ] && mv "$2" "$2.bak"
+    if [ "$USE_SYMLINK" == true ]; then
+          ln -s $1 $2
+    else
+          ln $1 $2
+    fi
+}
+
+link_config "$CONFIG_STORAGE/zshrc" "$HOME/.zshrc"
+link_config "$CONFIG_STORAGE/zshrc" "$HOME/.zshrc-root"
+
---
+++ zshrc
@@ -0,0 +1,7 @@
+export PATH=$HOME/bin:/usr/local/bin:$HOME/core/scripts:$PATH
+export ZSH=/home/sauron/.oh-my-zsh
+
+ZSH_THEME="agnoster"
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
