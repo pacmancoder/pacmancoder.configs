6d4fe497c597ef0051a71fbdcd84b0a3c18b56b5
Vladislav Nikonov <pacmancoder@gmail.com>
2019-01-11 00:16:21 +0200

    Updated various configs

--- install.sh
+++ install.sh
@@ -43,24 +43,99 @@ function ask_app {
     fi
 }
 
+DISTRO="unknown"
+
+function detect_distro
+{
+    display_topic "==== Detecting distro ===="
+    DIST_INFO=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+    if [[ $DIST_INFO == *"Solus"* ]]; then
+        DISTRO="solus"
+    elif [[ $DIST_INFO == *"Ubuntu"* ]]; then
+        DISTRO="ubuntu"
+    else
+        echo "Cant'd detect distro"
+        set -e
+    fi
+    display_success "Detected %DISTRO"
+}
+
+function install_package {
+    display_topic "==== Installing package %1 ===="
+    case $DISTRO in
+        solus)
+            sudo eopkg install -y "%1"
+        ;;
+        ubuntu)
+            sudo apt -y install "%1"
+        ;;
+    esac
+    display_success "Installed %1"
+}
+
+function install_dev_package {
+    display_topic "==== Installing dev-package %1 ===="
+    case $DISTRO in
+        solus)
+            sudo eopkg install -y "%1-devel"
+        ;;
+        ubuntu)
+            sudo apt -y install "%1-dev"
+        ;;
+    esac
+    display_success "Installed %1"
+}
+
+function install_build_essential {
+    display_topic "==== Installing build essential ===="
+    case $DISTRO in
+        solus)
+            sudo eopkg install sudo eopkg install -y -c system.devel
+        ;;
+        ubuntu)
+            sudo apt -y install build-essential
+        ;;
+    esac
+    display_success "Installed build-essential"
+}
+
 # ================== zsh =================
 ask_app "zsh"
 if [ -z $skip ]; then
+    install_package "zsh"
     link_config "$CONFIG_STORAGE/zsh/zshrc" "$HOME/.zshrc"
+    display_success "zsh installed"
 fi
 
 # ================ neovim ================
 ask_app "neovim"
 if [ -z $skip ]; then
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    install_package "neovim"
+    install_package "python3"
+    install_dev_package "python3"
+    install_package "cmake"
+    install_build_essential
+    curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     mkdir -p "$HOME/.config/nvim"
     link_config "$CONFIG_STORAGE/neovim/init.vim" "$HOME/.config/nvim/init.vim"
-    display_warn "Don't forget to run :PlugInstall and UpdateRemotePlugins!"
+    sudo pip3 install nvim
+    # Install plugins
+    nvim -c "q"
+    (
+        cd "$HOME/.config/nvim/plugged/youcompleteme"
+        python3 install.py --clang-completer
+    )
+    display_success "neovim installed"
 fi
 
 ask_app "tmux"
 if [ -z $skip ]; then
+    install_package "tmux"
+    git clone https://github.com/gpakosz/.tmux.git "$HOME/.tmux"
+    link_config "$HOME/.tmux/.tmux.conf" "$HOME/.tmux.conf"
     link_config "$CONFIG_STORAGE/tmux/tmux.conf.local" "$HOME/.tmux.conf.local"
-    display_warn "Don't forget to install oh-my-tmux"
+    display_success "tmux installed"
 fi
+
+display_success "Configuration finished!"
--- neovim/init.vim
+++ neovim/init.vim
@@ -4,6 +4,13 @@ Plug 'vim-airline/vim-airline-themes'
 Plug 'scrooloose/nerdtree'
 Plug 'chriskempson/base16-vim'
 Plug 'tpope/vim-fugitive'
+Plug 'tpope/vim-surround'
+Plug 'airblade/vim-gitgutter'
+Plug 'kien/ctrlp.vim'
+Plug 'scrooloose/nerdcommenter'
+Plug 'yggdroot/indentline'
+Plug 'raimondi/delimitmate'
+Plug 'valloric/youcompleteme'
 call plug#end()
 
 " Auto install plugins on first launch
@@ -11,16 +18,37 @@ if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
     PlugInstall
 endif
 
+" Colo scheme
 let base16colorspace=256
 colorscheme base16-ashes
 
-map <C-b> :NERDTreeToggle<CR>
+" Nerd tree
+map <F1> :NERDTreeToggle<CR>
 
+" More natural Tab key
+inoremap <S-Tab> <C-d>
+nnoremap <Tab> >>_
+nnoremap <S-Tab> <<_
+vnoremap <Tab> >gv
+vnoremap <S-Tab> <gv
+
+" Navigation in insert mode using Alt key
+inoremap <silent> <M-h> <Left>
+inoremap <silent> <M-j> <Down>
+inoremap <silent> <M-k> <Up>
+inoremap <silent> <M-l> <Right>
+
+" Editor display settings
+set list
+set colorcolumn=100
+set number relativenumber
+set nu rnu
+
+" Mouse settings
+set mouse=a
+
+" Indent settings
 set tabstop=4
 set softtabstop=0
 set expandtab
 set shiftwidth=4
-
-set list
-set colorcolumn=100
-set nu
